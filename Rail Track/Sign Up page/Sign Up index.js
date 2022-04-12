71f60d573d9f82acd6c27fc4f20b8e9d03af6dcb
P-Candy5 <malaprecious2002@gmail.com>
2022-04-12 03:23:05 -0700

    still designing the popular destinations

---
+++ Rail Track/Sign Up page/Sign Up index.js
@@ -0,0 +1,35 @@
+let get = (item) => document.querySelectorAll(item);
+let take = (item) => document.querySelector(item);
+
+let inputHolder = get("input");
+let buttonHolder = take("button");
+let error = get(".display-error");
+let messageHolder = take(".message");
+
+console.log(" do na");
+
+async function same(data) {
+  console.log("print out");
+  if (data.password === data.confirmPassword) {
+    error.classList.add("hide");
+    const resp = await fetch("/url", {
+      method: "POST",
+      body: data,
+    });
+  } else {
+    messageHolder.innerText = "incorrect Password!";
+    inputHolder[2].value = "";
+    inputHolder[3].value = "";
+  }
+}
+
+buttonHolder.addEventListener("click", (e) => {
+  e.preventDefault();
+  let fullName = inputHolder[0].value;
+  let email = inputHolder[1].value;
+  let password = inputHolder[2].value;
+  let confirmPassword = inputHolder[3].value;
+  const data = { fullName, email, password, confirmPassword };
+  console.log(data);
+  same(data);
+});
